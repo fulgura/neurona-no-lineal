c81c990cb29d324723583a74080dcfa67c4a492f
Diego Martín Correa <diegomartincorrea@gmail.com>
2011-09-29 13:53:10 -0300

    leer datos test completo

--- ejercicio1a.m
+++
@@ -1,5 +0,0 @@
-
-[P T CantPatrones CantAtrib] = leerDatos('Vinos.csv', true, 1, 2, 14);
-
-P = P(1:10,1:12);
-T = T(1:10,:);
---
+++ leerDatosTest.m
@@ -0,0 +1,4 @@
+clear all
+clc
+
+[P T CantPatrones CantAtrib] = leerDatos('clima_numerico.csv', true, 5, 1, 4)
